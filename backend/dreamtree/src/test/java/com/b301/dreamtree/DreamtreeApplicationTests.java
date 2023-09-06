8b2e107de43849020ffd56756410cae3af624fa5
김선진 <tjswls8062@skuniv.ac.kr>
2023-09-06 13:48:06 +0900

    Init: 초기 세팅

---
+++ backend/dreamtree/src/test/java/com/b301/dreamtree/DreamtreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b301.dreamtree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamtreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
