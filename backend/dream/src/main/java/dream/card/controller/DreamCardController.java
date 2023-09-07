9f722ea1126800f2121a18f8e28fb7ab31c48b23
김선진 <tjswls8062@skuniv.ac.kr>
2023-09-07 13:40:48 +0900

    Init: 컨트롤러, 예외 처리 로직 생성

--- backend/dream/src/main/java/dream/card/controller/DreamCardController.java
+++ backend/dream/src/main/java/dream/card/controller/DreamCardController.java
@@ -2,13 +2,23 @@ package dream.card.controller;
 
 
 import dream.card.service.DreamCardService;
+import dream.common.domain.ResultTemplate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/night")
 @RequiredArgsConstructor
 public class DreamCardController {
 
-    private final DreamCardService cardService;
+    private final DreamCardService dreamCardService;
 
+
+    @GetMapping(value = "/")
+    public ResultTemplate getNightMain(){
+        return dreamCardService.getNightMain();
+    }
 }
