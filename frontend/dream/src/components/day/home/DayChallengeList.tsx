39a10b55454ae2ba3d4145e5ca67e8f442d5e9bb
Minji Byun <jwww98730@naver.com>
2023-09-12 16:55:56 +0900

    [FE] Feat: 무한 스크롤 초기 데이터 및 props 세팅

--- frontend/dream/src/components/day/home/DayChallengeList.tsx
+++ frontend/dream/src/components/day/home/DayChallengeList.tsx
@@ -3,18 +3,48 @@
 // <ChallengeContentListItem></ChallengeContentListItem> ./daycommon 
 
 // 리액트
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // 컴포넌트
 import ChalContentListItem from "../daycommon/ChalContentListItem";
 
 // 스타일
+export interface DayChallengeObj {
+  title :string,
+  period :string,
+  challengeId :number
+}
+
+export interface DayChallengeList extends Array<DayChallengeObj> {}
+
 
 const DayChallengeList = () => {
+    const [allChalList, setAllChalList] = useState<null | DayChallengeList>([]);
+  
+    // .axios 연결 전 임의의 값을 allChalList에 넣어두기
+    let newObj :DayChallengeObj = {
+      title : "111",
+      period : "1일",
+      challengeId : 111,
+    }
+    let newObj2 :DayChallengeObj = {
+      title : "222",
+      period : "2일",
+      challengeId : 222,
+    }
+  
+  
+    // 처음 렌더링 시 Challenge List axios 요청
+    useEffect(() => {
+      setAllChalList([newObj, newObj2, newObj2, newObj2, newObj2, newObj2])
+    }, [])
 
   return (
     <>
-    <ChalContentListItem />
+    {
+      allChalList?.map((chal :DayChallengeObj) => (
+      <ChalContentListItem key={chal.challengeId} chal={chal} />))
+    }
     </>
   )
 }
