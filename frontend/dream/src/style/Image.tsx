70c029a0da5393ea0f1da7df56e1ebf902da6b66
Dongmin <dl_skflsalfm@naver.com>
2023-09-13 16:24:03 +0900

    [FE] Style: 옥션 카드 1차 시안 완료

--- frontend/dream/src/style/Image.tsx
+++ frontend/dream/src/style/Image.tsx
@@ -142,7 +142,7 @@ const StyledImage = styled.div<ImageProps>`
     ${(props) =>
       props.$nightImageBorder &&
       css`
-        border-radius: 10px;  
+        border-radius: 1rem;  
       `
     }
 
@@ -160,9 +160,23 @@ const StyledImage = styled.div<ImageProps>`
     ${(props) =>
       props.$auctionCard &&
       css`
-        width: 10rem;
-        height: 10rem;
-        margin: 1rem;
+        /* width: 10rem;
+        height: 10rem; */
+        width: 100%;
+        height: 100%;
+        /* margin: 0.5rem 1rem 1rem; */
+        margin: 0;
+        padding: 0 1rem;
+        background-color: inherit;
+        border-radius: 0;
+        & > img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          aspect-ratio: 1/1;
+          object-position: center;
+          border-radius: 1rem;
+        }
       `
     }
 
