ff6f74e89af3f3cd138ec0e1802b9369b2923285
bmsnc <qjatjs98@gmail.com>
2023-09-13 15:35:32 +0900

    Feat: 좋아요, 좋아요 취소 기능 구현 및 중복, 일치 예외 코드 추가

--- backend/dream/src/main/java/dream/advice/GlobalExceptionHandler.java
+++ backend/dream/src/main/java/dream/advice/GlobalExceptionHandler.java
@@ -1,14 +1,7 @@
 package dream.advice;
 
 import dream.common.domain.ResultTemplate;
-<<<<<<< HEAD
 import dream.common.exception.*;
-=======
-import dream.common.exception.InvalidAccessTokenException;
-import dream.common.exception.InvalidRefreshTokenException;
-import dream.common.exception.NoSuchElementException;
-import dream.common.exception.NotFoundException;
->>>>>>> back-develop
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
