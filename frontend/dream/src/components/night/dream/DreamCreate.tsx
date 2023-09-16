43d76a601d85c688c38dd0ed490030ec8acf5361
bmj0053 <bmj05378@gmail.com>
2023-09-16 13:54:56 +0900

    [FE] Design: DreamCreate 기본 세팅

--- frontend/dream/src/components/night/dream/DreamCreate.tsx
+++ frontend/dream/src/components/night/dream/DreamCreate.tsx
@@ -3,17 +3,62 @@
 // 공개 // 버튼 2개
 
 // 리액트
-import React from "react";
+import React, { useState } from "react";
 
 // 컴포넌트
-import Button from "components/common/Button";
 
 // 스타일
+import Button from "components/common/Button";
+import TextArea from "style/TextArea";
+import styled from "styled-components";
+
+const DreamCreateContainer = styled.div`
+  width: 100%;
+  margin: 6rem 0.5rem;
+  background-color: white;
+`;
 
 const DreamCreate = () => {
-
   return (
     <>
+    <DreamCreateContainer>
+      <Button
+      $fullWidth
+      $nightVoice
+      />
+      <TextArea
+      $nightDreamInput
+      />
+
+      <div
+      style={{
+        display: "flex", 
+        justifyContent: "space-between", 
+        padding: "0 1rem"
+      }}
+      >
+        <div>
+          {/* 임시 체크박스 */}
+          <input
+          type="checkbox"
+          />
+          공개
+        </div>
+
+        <div>
+          <Button
+          $nightMiddlePurple
+          >
+          취소
+          </Button>
+          <Button
+          $nightPurple
+          >
+          등록
+          </Button>
+        </div>
+      </div>
+    </DreamCreateContainer>
     </>
   )
 }
