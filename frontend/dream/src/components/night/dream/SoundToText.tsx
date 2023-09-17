59f47d68c172dc78f7e71606cbf9b9333f30bb38
bmj0053 <bmj05378@gmail.com>
2023-09-17 21:40:17 +0900

    [FE] Feat: SoundToText 파일 추가

---
+++ frontend/dream/src/components/night/dream/SoundToText.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react"
+// import { accentClickableState, accentSttState } from '/src/recoil/HW_Atom';
+// import { useRecoilState } from "recoil";
+
+let recognition: any = null
+if ("webkitSpeechRecognition" in window) {
+  recognition = new webkitSpeechRecognition()
+  recognition.continuous = true
+  recognition.lang = "ko-KR"
+}
+
+const SoundToText = () => {
+  const [accentText, setAccentText] = useState<string>("");
+  const [accentClickable, setaccentClickable] = useState<boolean>(false);
+  const [isListening, setIsListening] = useState(false)
+
+  const startListening = () => {
+    setAccentText("");
+    setIsListening(true);
+    recognition.start() ;
+  }
+
+  // 음성 -> 텍스트로 변환
+  const accenting = () => {
+    console.log("accenting 실행")
+    if(!recognition) return 
+    console.log('여기')
+    setaccentClickable(false);
+    recognition.onresult = (event: any) => {
+      console.log(event)
+      console.log(typeof event)
+      setAccentText(event.results[0][0].transcript);
+    }
+    setIsListening(false)
+    recognition.stop()
+    setaccentClickable(true)
+  }
+
+  const stopListening = () => {
+    if(recognition){recognition.stop()}
+    setIsListening(false)
+    setaccentClickable(true)
+  }
+
+  return {
+    isListening,
+    startListening,
+    accenting,
+    stopListening,
+    hasRecognitionSupport: !! recognition
+  }
+}
+
+export default SoundToText
