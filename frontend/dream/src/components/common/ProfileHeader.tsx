c45638943773619df0ad433dd913a95784fffad7
Minji Byun <jwww98730@naver.com>
2023-09-18 17:34:12 +0900

    [FE] Feat: Profile Header 사진 추가

--- frontend/dream/src/components/common/ProfileHeader.tsx
+++ frontend/dream/src/components/common/ProfileHeader.tsx
@@ -9,15 +9,34 @@
 
 // 리액트
 import React from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
 
 // 컴포넌트
-import Button from "./Button";
+
 // 스타일
+import Button from "./Button";
+import Wrap from "style/Wrap";
+import Image from "style/Image";
+import Text from "style/Text";
 
 const ProfileHeader = () => {
+  const themeMode = useSelector((state: RootState) => state.themeModeReducer.themeMode);
 
   return (
     <>
+    <Wrap>
+      <Image
+      $smallProfileImage
+      >
+        <img src="https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjAy/MDAxNjQzMTAyOTk2NjE0.gw_H_jjBM64svaftcnheR6-mHHlmGOyrr6htAuxPETsg.8JJSQNEA5HX2WmrshjZ-VjmJWqhmgE40Qm5csIud9VUg.JPEG.minziminzi128/IMG_7374.JPG?type=w800"/>
+      </Image>
+      <div>
+        <Text
+        
+        >나는프론트엔드</Text>
+      </div>
+    </Wrap>
     </>
   )
 }
