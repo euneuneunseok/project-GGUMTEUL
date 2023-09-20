e3126f8224477f86789fe6ed8106fbfd6e1200aa
Minji Byun <jwww98730@naver.com>
2023-09-20 12:39:26 +0900

    [FE] Comment: DayStoryDetailPage에 있는 컴포넌트 주석처리

--- frontend/dream/src/pages/day/home/DayStoryDetailPage.tsx
+++ frontend/dream/src/pages/day/home/DayStoryDetailPage.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 // 컴포넌트
 // 별도 헤더 정의 필수
-import DayStoryDetail from "components/day/home/DayStoryDetail";
+// import DayStoryDetail from "components/day/home/DayStoryDetail";
 
 // 스타일
 
@@ -13,7 +13,7 @@ const DayStoryDetailPage = () => {
 
   return (
     <>
-    <DayStoryDetail />
+    {/* <DayStoryDetail /> */}
     </>
   )
 }
