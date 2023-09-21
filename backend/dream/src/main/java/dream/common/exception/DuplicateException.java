5a7459e557aefce8f8fc26e53fbbc7f9733eb274
김선진 <tjswls8062@skuniv.ac.kr>
2023-09-21 09:57:05 +0900

    Feat: 좋아요 등록, 좋아요 취소 기능 구현 완료

--- backend/dream/src/main/java/dream/common/exception/DuplicateException.java
+++ backend/dream/src/main/java/dream/common/exception/DuplicateException.java
@@ -3,6 +3,7 @@ package dream.common.exception;
 public class DuplicateException extends RuntimeException{
 
     public static final String USER_LIKE_DUPLICATE = "이미 좋아요 한 유저 입니다.";
+    public static final String CHALLENGE_DETAIL_LIKE_DUPLICATE = "이미 해당 인증글을 좋아요 했습니다.";
     public static final String CHALLENGE_PARTICIPATION_DUPLICATE = "이미 참여 중인 챌린지 입니다.";
     public static final String NICKNAME_DUPLICATE = "이미 존재하는 닉네임입니다.";
     public static final String FOLLOW_DUPLICATE = "이미 팔로우 중인 회원입니다.";
