70b1fb06977862e887f065085c6f8a6136d17890
Dongmin <dl_skflsalfm@naver.com>
2023-09-22 17:53:54 +0900

    [Data] 데이터 초기 설정

---
+++ dataend/main.py
@@ -0,0 +1,24 @@
+# main.py
+
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "Hello FastAPI"
+
+# @app.get("/json")
+# def printJson():
+#     return {
+#         "Number": 12345
+#     }
+
+# class Post(BaseModel):
+#     title: str
+#     content: str
+
+# @app.post("/posts")
+# def createContents(post: Post):
+#     title: post.title
+#     content = post.content
