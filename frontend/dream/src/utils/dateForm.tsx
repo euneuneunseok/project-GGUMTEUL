959b35840fdee414006dece5cc4ad1e1916c860a
Dongmin <dl_skflsalfm@naver.com>
2023-09-22 11:03:39 +0900

    [FE] Fix: 연월일시분 형식 수정

--- frontend/dream/src/utils/dateForm.tsx
+++ frontend/dream/src/utils/dateForm.tsx
@@ -21,10 +21,22 @@ export const changeDateHour = (date: string | undefined) :string => {
   if (date === undefined) return "0"
     const createdDate = new Date(date)
     const year = createdDate.getFullYear()
-    const month = createdDate.getMonth() +1
-    const day = createdDate.getDate()
-    const hour = createdDate.getHours()
-    const miniutes = createdDate.getMinutes()
-    
+    let month :string = String(createdDate.getMonth() +1)
+    let day :string = String(createdDate.getDate())
+    let hour:string = String(createdDate.getHours())
+    let miniutes:string = String(createdDate.getMinutes())
+    if (Number(month) < 10) {
+      month = `0${month}`
+    }
+    if (Number(day) < 10) {
+      day = `0${day}`
+    }
+    if (Number(hour) < 10) {
+      hour = `0${hour}`
+    }
+    if (Number(miniutes) < 10) {
+      miniutes = `0${miniutes}`
+    }
+  
     return [year, month, day].join(".") + " " + [hour, miniutes].join(":")
 }
