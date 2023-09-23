6a4abd948d731fae9de505e25fb77f2b9e2728e5
bmj0053 <bmj05378@gmail.com>
2023-09-23 19:40:22 +0900

    [FE] Design: 낮 메인 카테고리 추가

--- frontend/dream/src/components/day/daycommon/DayCategoryList.tsx
+++ frontend/dream/src/components/day/daycommon/DayCategoryList.tsx
@@ -3,18 +3,33 @@
 // <CategoryBox></CategoryBox>
 
 // 리액트
+import Button from "components/common/Button";
 import React from "react";
 import Text from "style/Text";
+import styled from "styled-components";
 
 // 컴포넌트
 
 // 스타일
+const CategoryWrap = styled.div`
+`
 
 const DayCategoryList = () => {
+  let categoryList :string[] = [
+    '재물', '진로', '인간관계', '감정', '자기계발', 
+    '건강', '도전', '교양', '학습', '기타'];
 
   return (
     <>
-    <Text $isBold>카테고리</Text>
+    <CategoryWrap>
+      <Text $isBold>카테고리</Text>
+      {
+        categoryList.map((category, i) => 
+          <Button $category key={i}>{category}</Button>
+        
+        )
+      }
+    </CategoryWrap>
     </>
   )
 }
