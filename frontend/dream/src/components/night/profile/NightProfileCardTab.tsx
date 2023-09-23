7d2553fe6eccbe4ed5ee5b240478297e923d70b4
bmj0053 <bmj05378@gmail.com>
2023-09-23 16:42:11 +0900

    [FE] Design: 밤 프로필 카드 탭 기본 디자인 세팅

--- frontend/dream/src/components/night/profile/NightProfileCardTab.tsx
+++ frontend/dream/src/components/night/profile/NightProfileCardTab.tsx
@@ -7,13 +7,45 @@ import React from "react";
 
 // 컴포넌트
 import Button from "components/common/Button";
+import Image from "style/Image";
+import Wrap from "style/Wrap";
+import styled from "styled-components";
 
 // 스타일
+const ProfileCardButtonWrap = styled.div`
+  margin: 1rem 0.5rem;
+`
+
+const ProfileDreamCardWrap = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin: 0 0.5rem;
+`
 
 const NightProfileCardTab = () => {
 
   return (
     <>
+    <ProfileCardButtonWrap>
+      {/* 공개, 구매 버튼 */}
+      <Button $follow $nightPalePurple>공개</Button>
+      <Button $follow $nightPalePurple>구매</Button>
+    </ProfileCardButtonWrap>
+
+    <ProfileDreamCardWrap>
+      <Image $profileCard $nightImageBorder>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL1FLLXoD2D9jTPPy5nohrnuFBE0RypC2bdJucBEMGTQ&s"></img>
+      </Image>
+      <Image $profileCard $nightImageBorder>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL1FLLXoD2D9jTPPy5nohrnuFBE0RypC2bdJucBEMGTQ&s"></img>
+      </Image>
+      <Image $profileCard $nightImageBorder>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL1FLLXoD2D9jTPPy5nohrnuFBE0RypC2bdJucBEMGTQ&s"></img>
+      </Image>
+      <Image $profileCard $nightImageBorder>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQL1FLLXoD2D9jTPPy5nohrnuFBE0RypC2bdJucBEMGTQ&s"></img>
+      </Image>
+    </ProfileDreamCardWrap>
     </>
   )
 }
