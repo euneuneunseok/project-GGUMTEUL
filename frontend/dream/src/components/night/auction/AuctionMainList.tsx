514c92b5c5179d33eca1d059f3639eb9498e9eb9
bmj0053 <bmj05378@gmail.com>
2023-09-25 20:53:49 +0900

    [FE] Feat: 경매 메인 리스트 axios 연결, 무한 스크롤 추가

--- frontend/dream/src/components/night/auction/AuctionMainList.tsx
+++ frontend/dream/src/components/night/auction/AuctionMainList.tsx
@@ -12,6 +12,7 @@ import tokenHttp from "api/tokenHttp";
 // 스타일
 import Wrap from "style/Wrap";
 import { useNavigate } from "react-router-dom";
+import InfiniteScroll from "components/common/InfiniteScroll";
 
 export interface AuctionMainType {
   color : string
@@ -33,17 +34,51 @@ export interface AuctionCardType {
 export interface AuctionListType extends Array<AuctionCardType> {}
 
 const AuctionMainList = () => {
-  const [auctionList, setAuctionList] = useState<AuctionListType>([])
-  const size = 10
+  const [auctionList, setAuctionList] = useState<AuctionListType>([]);
+  const [lastItemId, setLastItemId] = useState<number>(-1);
+  const size = 6;
+
+  // axios 요청
+  const getAxios = () => {
+    let apiAddress :string = "";
+    
+    // 처음 요청 시
+    if (lastItemId === -1) {apiAddress = `/auction/list?size=${size}`}
+    // 두번째부터 요청
+    else {apiAddress = `/auction/list?lastItemId=${lastItemId}&size=${size}`}
+    
+    tokenHttp.get(apiAddress)
+    .then(res => {
+      if (typeof res.data.data.list === "object") {
+        setAuctionList([...auctionList, ...res.data.data.list])
+        console.log(res.data.data.list, "옥션리스트")
+      }
+    })
+  }
 
   useEffect(() => {
-    tokenHttp.get(`/auction/list?size=${size}`)
-    .then(res => {
-      setAuctionList(res.data.data.list)
-      console.log(res.data.data.list, "옥션리스트")
-    })
+    getAxios();
   }, [])
+
+  // lastItemId 업데이트
+  useEffect(() => {
+    auctionList[auctionList.length - 1] &&
+    setLastItemId(auctionList[auctionList.length - 1].auctionId)
+    console.log('lastItem : ', lastItemId)
+  }, [setAuctionList, auctionList])
+
+
   // 무한 스크롤 부분
+  const [arriveEnd, setArriveEnd] = useState<boolean>(false);
+
+  // 바닥에 다다랐으면 axios 요청
+  useEffect(() => {
+    if (arriveEnd) {
+      getAxios();
+      setArriveEnd(false);
+    }
+  }, [arriveEnd])
+
 
 
   return (
@@ -51,9 +86,14 @@ const AuctionMainList = () => {
       <SearchBar onChange={()=>console.log("짠")} />
       <Wrap $auctionCardWrap>
         {
-          auctionList.map((item, idx) => (
-            <AuctionCard auctionCard={item} key={item.auctionId} />
-          ))
+          auctionList &&
+          <InfiniteScroll 
+          setArriveEnd={setArriveEnd}
+          component={
+            auctionList.map((item, idx) => (
+              <AuctionCard auctionCard={item} key={item.auctionId} />
+            ))}
+          />
         }
       </Wrap>
     </>
