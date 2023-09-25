8b25133af8524df8e2e7fbc79c023c736bec1be5
이 은 석 <modelism93@naver.com>
2023-09-25 14:57:49 +0900

    [FE] Feat: manage Challenge에 progress bar 형태 잡음

--- frontend/dream/src/components/day/challenge/manage/ChalManageDetail.tsx
+++ frontend/dream/src/components/day/challenge/manage/ChalManageDetail.tsx
@@ -27,7 +27,49 @@ import { useParams } from "react-router";
 import basicHttp from "api/basicHttp";
 import { ChalDetailDataType, ChalDetailInfoProps } from "../ChalDetail";
 import Wrap from "style/Wrap";
+import styled from "styled-components";
+import tokenHttp from "api/tokenHttp";
 // 스타일
+
+
+// progress 속성을 정의
+interface ProgressBarProps {
+  progress :number;
+}
+
+const ProgressBar = styled.div<ProgressBarProps>`
+  height: 0.8rem;
+  margin-right: 0.75rem;
+  border-radius: 1rem;
+  color: #997ad8;
+  position: relative;
+  background-color: #F9F9F9;
+
+  & > p {
+    font-size: 0.8rem;
+    z-index: 10;
+    color: black;
+  }
+  
+  &::before {
+    content: "";
+    position: absolute;
+    inset: 0 calc(${props => 100 -props.progress}%) 0 0;
+    border-radius: inherit;
+    background: currentColor;
+    animation: p6 2s;
+  }
+  @keyframes p6 {
+    /* 100% {inset:0} */
+    0% {
+      width: 0;
+    }
+    100% {
+      width: ${props => props.progress}%; // 원하는 최대 너비
+    }
+  }
+`
+
 
 // 챌린지 디테일 초기값
 const initialChalDetail:ChalDetailInfoProps = {
@@ -53,12 +95,18 @@ const ChalManageDetail = () => {
   const currentChallengeId = Number(params.challengeId)
   const [chalData, setChalData] = useState<ChalDetailDataType>()
 
+  const [progress, setProgress] = useState<number>(70); // 꿈틀도 추후 변경하기
+  
   useEffect(()=>{
     // 렌더링되었을 때 참여
-    basicHttp.get(`day/challenge/item/${currentChallengeId}`)
+    tokenHttp.get(`day/challenge/item/${currentChallengeId}`)
       .then((response)=>{
         const res = response.data.data
         setChalData(res.detail)
+        console.log(chalData)
+        // 성취도 계산
+        
+        // 현재 시작 후 몇일인지 계산
 
       })
       .catch((e)=>{console.log(e)})
@@ -90,7 +138,10 @@ const ChalManageDetail = () => {
       </Container>
 
       {/* 성취도 바 */}
-      
+      <ProgressBar progress={progress}>
+        {/* <BoxTitle $boxTistle>성취도</BoxTitle> */}
+        <p>{progress}</p>
+      </ProgressBar>  
 
     </Container>
   )
