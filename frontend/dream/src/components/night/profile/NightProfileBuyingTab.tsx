764914e13134c37ad9eba81f5e409c778d0dc37f
Minji Byun <jwww98730@naver.com>
2023-09-25 10:55:16 +0900

    [FE] Feat: 꿈 사는 탭 axios 추가

--- frontend/dream/src/components/night/profile/NightProfileBuyingTab.tsx
+++ frontend/dream/src/components/night/profile/NightProfileBuyingTab.tsx
@@ -1,21 +1,69 @@
-// 꿈 사는중 옥션 Array.map()
-
-{/* <AuctionCard></AuctionCard>
-<AuctionCard></AuctionCard> */}
-
 // 리액트
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // 컴포넌트
 import AuctionCard from "../nightcommon/AuctionCard";
+import basicHttp from "api/basicHttp";
+import InfiniteScroll from "components/common/InfiniteScroll";
+import { AuctionCardType } from "../auction/AuctionMainList";
 
 // 스타일
+// export interface AuctionBuyingAxiosType {
+//   dreamCardId :number,
+//   dreamCardImageUrl :string, 
+//   positivePoint :string,
+//   rarePoint :string,
+//   keywords :string[],
+//   endedAt :string,
+//   auctionStatus :string,
+// }
+
 
 const NightProfileBuyingTab = () => {
 
+  // axios로 데이터 받기
+  // const [auctionBuyingDataList, setAuctionBuyingDataList] = useState<AuctionBuyingAxiosType[]>();
+  const [auctionBuyingDataList, setAuctionBuyingDataList] = useState<AuctionCardType[]>();
+  const [lastItemId, setLastItemId] = useState<number>(0);
+  let size = 12;
+  
+  const getAxios = () => {
+    basicHttp.get(`/profile/night/auction/participation/list?lastItemId=${lastItemId}&$size={size}`)
+    .then((res) => {
+      console.log("== 꿈 받기 탭 ==", res); 
+      setAuctionBuyingDataList(res.data.data);
+    })
+    .catch((err) => console.log("== 꿈 받기 탭 ==", err))
+  }
+
+  useEffect(() => {
+    getAxios();
+  }, []);
+
+  // infinite scroll
+  const [arriveEnd, setArriveEnd] = useState<boolean>(false); // 바닥에 다다름을 알려주는 변수
+
+  useEffect(() => {
+    // 바닥에 다다랐으면 axios 요청
+    if (arriveEnd) {
+      // axios 요청
+      getAxios();
+      // setArriveEnd(false);
+      // setLastItemId(auctionBuyingDataList[-1]["challengeId"]); // 마지막 item id 변경
+    }
+  }, [arriveEnd])
+
   return (
     <>
-    {/* <AuctionCard/> */}
+    <InfiniteScroll 
+    setArriveEnd={setArriveEnd} 
+    // lastItemId={lastItemId}
+    component={
+      auctionBuyingDataList?.map((data, i) => (
+        <AuctionCard auctionCard={data} key={i}/>
+      ))
+    }
+    />
     </>
   )
 }
