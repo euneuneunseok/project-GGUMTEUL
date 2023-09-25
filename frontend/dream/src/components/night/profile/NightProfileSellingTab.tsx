bd65e67bac11480a14e492a2e9ee4e9d624a0e67
Minji Byun <jwww98730@naver.com>
2023-09-25 10:55:30 +0900

    [FE] Feat: 꿈 파는 탭 axios 추가

--- frontend/dream/src/components/night/profile/NightProfileSellingTab.tsx
+++ frontend/dream/src/components/night/profile/NightProfileSellingTab.tsx
@@ -1,22 +1,59 @@
-// 꿈 파는중 옥션 Array.map()
-
-{/* <AuctionCard></AuctionCard>
-<AuctionCard></AuctionCard> */}
-
 // 리액트
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // 컴포넌트
 import AuctionCard from "../nightcommon/AuctionCard";
+import basicHttp from "api/basicHttp";
+import InfiniteScroll from "components/common/InfiniteScroll";
+import { AuctionCardType } from "../auction/AuctionMainList";
 
 // 스타일
 
 const NightProfileSellingTab = () => {
 
+  // axios로 데이터 받기
+  // const [auctionSellingDataList, setAuctionSellingDataList] = useState<AuctionSellingAxiosType[]>();
+  const [auctionSellingDataList, setAuctionSellingDataList] = useState<AuctionCardType[]>();
+  const [lastItemId, setLastItemId] = useState<number>(0);
+  let size = 12;
+  
+  const getAxios = () => {
+    basicHttp.get(`/profile/night/auction/list?lastItemId=${lastItemId}&size=${size}`)
+    .then((res) => {
+      console.log("== 꿈 팔기 탭 ==", res); 
+      setAuctionSellingDataList(res.data.data);
+    })
+    .catch((err) => console.log("== 꿈 팔기 탭 ==", err))
+  }
+
+  useEffect(() => {
+    getAxios();
+  }, []);
+
+  // infinite scroll
+  const [arriveEnd, setArriveEnd] = useState<boolean>(false); // 바닥에 다다름을 알려주는 변수
+
+  useEffect(() => {
+    // 바닥에 다다랐으면 axios 요청
+    if (arriveEnd) {
+      // axios 요청
+      getAxios();
+      // setArriveEnd(false);
+      // setLastItemId(auctionSellingDataList[-1]["challengeId"]); // 마지막 item id 변경
+    }
+  }, [arriveEnd])
+
   return (
     <>
-
-    {/* <AuctionCard /> */}
+    <InfiniteScroll 
+    setArriveEnd={setArriveEnd} 
+    // lastItemId={lastItemId}
+    component={
+      auctionSellingDataList?.map((data, i) => (
+        <AuctionCard auctionCard={data} key={i}/>
+      ))
+    }
+    />
     </>
   )
 }
