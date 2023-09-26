d41eb0452e506f8c2298f6e206292bff80b57255
Minji Byun <jwww98730@naver.com>
2023-09-26 15:50:06 +0900

    [FE] Fix: 낮 프로필 완료탭 lastItemId 로직 수정

--- frontend/dream/src/components/day/profile/DayProfileCompleteTab.tsx
+++ frontend/dream/src/components/day/profile/DayProfileCompleteTab.tsx
@@ -18,22 +18,23 @@ const DayProfileCompleteTab = () => {
   const [lastItemId, setLastItemId] = useState<number>(-1); // 마지막 아이템 번호
   // const profileUserId = useParams();
   const profileUserId :number = 20;
-  let size :number = 6;
+  let size :number = 10;
   
   // api 요청
   const getAxios = () => {
     let apiAddress :string = "";
 
     // 처음 요청 받을 때 : lastItemId 없음
-    if (lastItemId === -1) {apiAddress = `/day/mychallenge/end/list/${profileUserId}/?size=${size}`}
+    if (lastItemId === -1) {apiAddress = `/profile/day/mychallenge/end/list/${profileUserId}?size=${size}`}
     // 두번째부터 요청 할 때
-    else {apiAddress = `/day/mychallenge/end/list/${profileUserId}?lastItemId=${lastItemId}&size=${size}`}
+    else if (lastItemId > -1) {apiAddress = `/profile/day/mychallenge/end/list/${profileUserId}?lastItemId=${lastItemId}&size=${size}`}
     
     tokenHttp.get(apiAddress)
     .then((res)=> {
       console.log(res)
       if (typeof res.data.data.challengeList === "object") {
         setAllChalList([...allChalList, ...res.data.data.challengeList]);
+        setLastItemId(res.data.data.challengeList[res.data.data.challengeList.length - 1].challengeId)
       }
     })
     .catch(err=>console.log("DayProfileCompleteTab", err))
@@ -43,21 +44,17 @@ const DayProfileCompleteTab = () => {
     getAxios();
   }, [])
 
-  // lastItemId 업데이트
-  useEffect(() => {
-    allChalList[allChalList.length - 1] && 
-    setLastItemId(allChalList[allChalList.length - 1].challengeId)
-  }, [setAllChalList, allChalList])
 
   // infinite scroll
   const [arriveEnd, setArriveEnd] = useState<boolean>(false); // 바닥에 다다름을 알려주는 변수
 
   useEffect(() => {
     // 바닥에 다다랐으면 axios 요청
+    console.log("arrive End : ", arriveEnd)
     if (arriveEnd) {
       getAxios();
-      setArriveEnd(false);
     }
+    setArriveEnd(false);
   }, [arriveEnd])
 
 
