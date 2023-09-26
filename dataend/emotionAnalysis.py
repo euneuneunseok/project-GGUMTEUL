0c205f96846787291c414ab8747ba9fe2c94519d
Dongmin <dl_skflsalfm@naver.com>
2023-09-26 17:52:13 +0900

    [DE] Feat: 1-idx 700번까지 완료

--- dataend/emotionAnalysis.py
+++ dataend/emotionAnalysis.py
@@ -1,4 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import asyncio
+import json
 from google.cloud import language_v2
 from google.oauth2 import service_account
 
@@ -11,10 +14,11 @@ credentials = service_account.Credentials.from_service_account_file(
 )
 
 # 감정 분석할 텍스트
-text = "비둘기가 방에 들어옵니다."
+dream = "비둘기가 방에 들어옵니다."
 
 # 비동기 함수로 변환
-async def analyze_text_sentiment():
+async def analyze_text_sentiment(text):
+
     # NL API 클라이언트
     client = language_v2.LanguageServiceAsyncClient(credentials=credentials)
 
@@ -33,105 +37,54 @@ async def analyze_text_sentiment():
     # 감정 스코어
     sentiment = response.document_sentiment
 
-    print("감정 종합")
-    print(sentiment)
+    # 감정 점수 return하기
+    return sentiment.score
 
-    print("감정 점수")
-    print(f'{sentiment.score}')
-    print("강도")
-    print(f'{sentiment.magnitude}')
+
+# 긍정 점수, 부정 점수 만들기.
+def estimateScore(score):
+    positivePoint = 50 + round(score*50)
+    negativePoint = 50 - round(score*50)
+
+    return [positivePoint, negativePoint]
 
 # 비동기 함수 실행
-if __name__ == "__main__":
-    asyncio.run(analyze_text_sentiment())
+# if __name__ == "__main__":
+#     asyncio.run(analyze_text_sentiment(dream))
+
+input_path = ".\\addKeywordsMod\\addKeywordsMod1.json"
+with open(input_path, 'r', encoding='utf-8') as file:
+    keyData = json.load(file)
+output_path = ".\\addEmotion\\addEmotion1.json"
+
+idx = 0
+for data in keyData:
+    idx+=1
+    dreamOriginScore = asyncio.run(analyze_text_sentiment(data["dream"]))
+    tellingOriginScore = asyncio.run(analyze_text_sentiment(data["analysis"]["dreamTelling"]))
+
+    dreamPositivePoint, dreamNegativePoint = estimateScore(dreamOriginScore)
+    dreamTellingPositivePoint, dreamTellingNegativePoint = estimateScore(tellingOriginScore)
+
+    data["analysis"]["dreamPositivePoint"] = dreamPositivePoint
+    data["analysis"]["dreamNegativePoint"] = dreamNegativePoint
+    data["analysis"]["dreamTellingPositivePoint"] = dreamTellingPositivePoint
+    data["analysis"]["dreamTellingNegativePoint"] = dreamTellingNegativePoint
+
+    if idx % 100 == 0:
+        with open(output_path, 'w', encoding='utf-8') as out:
+            json.dump(keyData[:idx], fp=out, ensure_ascii=False, indent=2)
+        print(idx, "번째까지 완료")
+    print("#####")
+
+
+print(keyData[0])
+print(keyData[1])
+print(keyData[2])
+
+
+    
+# exit()
+
 
 exit()
-
-# 구글 키 설정
-key_path = ".\\googleKey.json"
-
-# 인증 설정
-credentials = service_account.Credentials.from_service_account_file(
-    key_path, scopes=['https://www.googleapis.com/auth/cloud-platform']
-)
-
-
-async def analyze_sentiment(text_content:str = text) -> None:
-    """
-    Analyzes Sentiment in a string.
-
-    Args:
-      text_content: The text content to analyze.
-    """
-    client = language_v2.LanguageServiceAsyncClient(credentials=credentials)
-
-    # Available Type : PlainText, HTML
-    document_type_in_plain_text = language_v2.Document.Type.PLAIN_TEXT
-
-    # Optional. If not specified, the language is automatically detected.
-    # For list of supported languages:
-    # https://cloud.google.com/natural-language/docs/languages
-
-    language_code = "ko"
-    document = {
-        "content": text_content,
-        "type_": document_type_in_plain_text,
-        "language_code": language_code,
-    }
-
-    # Available values: NONE, UTF8, UTF16, UTF32
-    # See https://cloud.google.com/natural-language/docs/reference/rest/v2/EncodingType.
-    encoding_type = language_v2.EncodingType.UTF8
-
-    response = await client.analyze_sentiment(
-        request={"document": document, "encoding_type": encoding_type}
-    )
-    # Get overall sentiment of the input document
-    print(f"Document sentiment score: {response.document_sentiment.score}")
-    print(f"Document sentiment magnitude: {response.document_sentiment.magnitude}")
-    # Get sentiment for all sentences in the document
-    for sentence in response.sentences:
-        print(f"Sentence text: {sentence.text.content}")
-        print(f"Sentence sentiment score: {sentence.sentiment.score}")
-        print(f"Sentence sentiment magnitude: {sentence.sentiment.magnitude}")
-
-    # Get the language of the text, which will be the same as
-    # the language specified in the request or, if not specified,
-    # the automatically-detected language.
-    print(f"Language of the text: {response.language_code}")
-
-
-# 감정분석할 텍스트
-
-# NL API 클라이언트
-# client = language_v2.LanguageServiceAsyncClient(credentials=credentials)
-
-# # 문서 생성
-# document = language_v2.Document(
-#     content=text, 
-#     type_=language_v2.Document.Type.PLAIN_TEXT,
-#     language_code="ko"
-#     )
-
-
-# request={"document": document, "encoding_type": language_v2.EncodingType.UTF8}
-
-# # 감정분석 요청
-# response = client.analyze_sentiment(request=request)
-
-# print(response, "응답답답")
-
-# # 감정 스코어
-# sentiment = response.document_sentiment
-
-# print("감정 종합")
-# print(sentiment)
-
-# print("감정점수")
-# print(f'{sentiment.score}')
-# print("강도")
-# print(f'{sentiment.score}')
-
-# 비동기 함수 실행
-if __name__ == "__main__":
-    asyncio.run(analyze_sentiment())
