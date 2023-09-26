9256d43eecdb88b383922405d24cfd6aa002652e
Dongmin <dl_skflsalfm@naver.com>
2023-09-26 13:04:06 +0900

    [DE] 깃 무시 테스트

---
+++ dataend/emotionAnalysis.py
@@ -0,0 +1,47 @@
+from google.cloud import language_v2
+
+content = "나는 행복할까요 너는 행복합니까 한화는 언제 우승합니까"
+
+def analyze_sentiment(text_content:str = content) -> None:
+    """
+    Analyzes Sentiment in a string.
+
+    Args:
+      text_content: The text content to analyze.
+    """
+    client = language_v2.LanguageServiceAsyncClient()
+
+    # Available Type : PlainText, HTML
+    document_type_in_plain_text = language_v2.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+
+    language_code = "ko"
+    document = {
+        "content": text_content,
+        "type_": document_type_in_plain_text,
+        "language_code": language_code,
+    }
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    # See https://cloud.google.com/natural-language/docs/reference/rest/v2/EncodingType.
+    encoding_type = language_v2.EncodingType.UTF8
+
+    response = client.analyze_sentiment(
+        request={"document": document, "encoding_type": encoding_type}
+    )
+    # Get overall sentiment of the input document
+    print(f"Document sentiment score: {response.document_sentiment.score}")
+    print(f"Document sentiment magnitude: {response.document_sentiment.magnitude}")
+    # Get sentiment for all sentences in the document
+    for sentence in response.sentences:
+        print(f"Sentence text: {sentence.text.content}")
+        print(f"Sentence sentiment score: {sentence.sentiment.score}")
+        print(f"Sentence sentiment magnitude: {sentence.sentiment.magnitude}")
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(f"Language of the text: {response.language_code}")
