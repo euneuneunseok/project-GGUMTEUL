e873616a8de732f7000931430bfc09eb1b758b78
Min <dl_skflsalfm@naver.com>
2023-09-27 14:18:44 +0900

    [DE] Feat: CORS 코드

--- dataend/main.py
+++ dataend/main.py
@@ -1,12 +1,16 @@
 # main.py
 
 from fastapi import FastAPI 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 from typing import List
 import httpx 
 import asyncio # 비동기 통신
 import requests
 
 from pydantic import BaseModel
+
 
 # 꿈 데이터
 class DreamModel(BaseModel):
@@ -15,6 +19,17 @@ class DreamModel(BaseModel):
     isShow: bool
 
 app = FastAPI()
+
+# 모든 도메인 허용
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인에서의 요청을 허용하려면 ["*"]로 설정
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드를 허용하려면 ["*"]로 설정
+    allow_headers=["*"],  # 모든 헤더를 허용하려면 ["*"]로 설정
+)
+
+# 자바 URL
 URL = "https://j9b301.p.ssafy.io/api"
 
 # 홈
