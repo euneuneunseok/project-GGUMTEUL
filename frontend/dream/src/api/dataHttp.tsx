8f65eb5e6e3c8af23229afd79878effdc70c6aa1
Dongmin <dl_skflsalfm@naver.com>
2023-09-27 14:44:08 +0900

    [FE] Feat: 생성 확인용

--- frontend/dream/src/api/dataHttp.tsx
+++ frontend/dream/src/api/dataHttp.tsx
@@ -3,8 +3,8 @@ import axios from "axios";
 export default axios.create({
   // baseURL: process.env.REACT_APP_LOCAL_URL,
   // baseURL: process.env.REACT_APP_SERVER_URL,
-  baseURL: "http://localhost:8000/data",
-  // baseURL : "http://localhost:9090/api",
+  baseURL: "https://j9b301.p.ssafy.io/data",
+  // baseURL : "http://localhost:8000/data",
 
   // 서로 다른 도메인(크로스 도메인)에 요청을 보낼 때 요청에 credential 정보를 담아서 보낼 지를 결정하는 항목
   withCredentials: false,
