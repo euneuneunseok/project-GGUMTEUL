f29ed48a8a7e4b464ecebaecfa9a92c9b8d216b9
Min <dl_skflsalfm@naver.com>
2023-09-27 13:23:50 +0900

    [DE] Feat: 홈 설정

--- dataend/main.py
+++ dataend/main.py
@@ -1,34 +1,42 @@
 # main.py
 
 from fastapi import FastAPI 
+from typing import List
 import httpx
 import asyncio
 
 from pydantic import BaseModel
 
+# 꿈 데이터
+class DreamModel(BaseModel):
+    dreamCardContent: str
+    dreamCardAuthor: int
+    isShow: bool
+
 app = FastAPI()
-URL = "https://j9b301.p.ssafy.io/api/datatest/a"
+URL = "https://j9b301.p.ssafy.io/api"
 
 # class Item(BaseModel):
 
-
+# 테스트 용도
 @app.get("/data")
 def root():
     return "Hello FastAPI"
+@app.get("/")
+def root():
+    return "Hello FastAPI"
 
-# res = requests.get(URL)
-# res.text
-
-exit()
+# exit()
 
 async def request(client):
     response = await client.get(URL)
     return response.text
 
-@app.get("/data/test")
-async def testReq():
-    async with httpx.AsyncClient() as client:
-        response = await client.get(URL)
-        print(response, "####")
-        return response
-    return response
+@app.post("/data/night/dream/create")
+async def dreamProcessing(data: DreamModel):
+    # 받은 데이터 처리
+    dreamCardContent = data.dreamCardContent
+    dreamCardAuthor: data.dreamCardAuthor
+    isShow = "T" if data.isShow else "F"
+
+
