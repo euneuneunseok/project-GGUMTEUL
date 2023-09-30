d79d731eea4c8312acdc2226201dbeb68d35f7f5
bmj0053 <bmj05378@gmail.com>
2023-09-30 17:05:44 +0900

    [FE] Fix: 낮 뱃지 탭 유저 연결 및 뱃지 없을 때 메세지 추가

--- frontend/dream/src/components/day/profile/DayProfileBadgeTab.tsx
+++ frontend/dream/src/components/day/profile/DayProfileBadgeTab.tsx
@@ -8,6 +8,8 @@ import InfiniteScroll from "components/common/InfiniteScroll";
 // 스타일
 import Image from "style/Image";
 import styled from "styled-components";
+import { useParams } from "react-router-dom";
+import Text from "style/Text";
 
 export interface ProfileBadgeAxiosType {
   badgeId :number,
@@ -21,40 +23,51 @@ const BadgeWrap = styled.div`
   flex-direction: column;
   margin: 0 0.5rem;
 `
+const NoBadgeMsgWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+`
 
 const DayProfileBadgeTab = () => {
+  const params = useParams();
+
   const [allBadgeList, setAllBadgeList] = useState<ProfileBadgeAxiosType[]>([]);
   const [lastItemId, setLastItemId] = useState<number>(-1); // 마지막 아이템 번호
-  let userId :number = 1;
+  const [hasNext, setHasNext] = useState<boolean>(true); 
+  const [noBadgeMsg, setNoBadgeMsg] = useState<string>("뱃지가 없습니다.");
   let size :number = 6;
 
-   // api 요청
+  // infinite scroll
+  const [arriveEnd, setArriveEnd] = useState<boolean>(true); // 바닥에 다다름을 알려주는 변수
+
+  // api 요청
   const getAxios = () => {
     let apiAddress :string = "";
 
     // 처음 요청 받을 때 : lastItemId 없음
-    if (lastItemId === -1) {apiAddress = `/profile/day/badge/list/${userId}?size=${size}`}
+    if (lastItemId === -1) {apiAddress = `/profile/day/badge/list/${params.userId}?size=${size}`}
     // 두번째부터 요청 할 때
-    else {apiAddress = `/profile/day/badge/list/${userId}?lastItemId=${lastItemId}&size=${size}`}
+    else {apiAddress = `/profile/day/badge/list/${params.userId}?lastItemId=${lastItemId}&size=${size}`}
     
-    tokenHttp.get(apiAddress)
-    .then((res)=> {
-      console.log(res.data.data.badgeList)
-      if (typeof res.data.data.badgeList === "object") {
-        setAllBadgeList([...allBadgeList, ...res.data.data.badgeList]);
-        setLastItemId(res.data.data.badgeList[res.data.data.badgeList.length - 1].badgeId)
-      }
-    })
-    .catch(err=>console.log("DayProfileBadgeTab : ", err))
+    if (arriveEnd && hasNext) {
+      tokenHttp.get(apiAddress)
+      .then((res)=> {
+        const response = res.data.data
+        const badgeList = response.badgeList
+        setAllBadgeList([...allBadgeList, ...badgeList]);
+        setLastItemId(badgeList[badgeList.length - 1]?.badgeId);
+        setHasNext(response.hasNext);
+
+        console.log("뱃지 탭 : ", res)
+      })
+      .catch(err=>console.log("뱃지 탭 : ", err))
+    }
   };
 
   useEffect(() => {
     getAxios();
   }, [])
-
-
-  // infinite scroll
-  const [arriveEnd, setArriveEnd] = useState<boolean>(false); // 바닥에 다다름을 알려주는 변수
 
   useEffect(() => {
     // 바닥에 다다랐으면 axios 요청
@@ -66,24 +79,29 @@ const DayProfileBadgeTab = () => {
 
   return (
     <>
-    <BadgeWrap>
       {
-        allBadgeList &&
-        <InfiniteScroll
-        setArriveEnd={setArriveEnd} 
-        // lastItemId={lastItemId}
-        component={
-          allBadgeList?.map((badge :ProfileBadgeAxiosType) => (
-            <Image 
-            $badge
-            key={badge.badgeId}><img src=""></img></Image>
-            // <ChalContentListItem key={badge.badgeId} badge={badge} />)
-            ))
-          }
-          >
-        </InfiniteScroll>
+        allBadgeList.length === 0
+        ? 
+        <NoBadgeMsgWrap>
+          <Text $black>{noBadgeMsg}</Text>
+        </NoBadgeMsgWrap>
+        :
+          <BadgeWrap>
+            <InfiniteScroll
+            setArriveEnd={setArriveEnd} 
+            // lastItemId={lastItemId}
+            component={
+              allBadgeList?.map((badge :ProfileBadgeAxiosType) => (
+                <Image 
+                $badge
+                key={badge.badgeId}><img src=""></img></Image>
+                // <ChalContentListItem key={badge.badgeId} badge={badge} />)
+                ))
+              }
+              >
+            </InfiniteScroll>
+          </BadgeWrap>
       }
-    </BadgeWrap>
     </>
   )
 }
