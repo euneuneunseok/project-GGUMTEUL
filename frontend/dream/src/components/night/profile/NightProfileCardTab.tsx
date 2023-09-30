f82aa736da75f536fcac0be55640ceabbe7d7450
bmj0053 <bmj05378@gmail.com>
2023-09-30 16:17:19 +0900

    [FE] Fix: 밤 꿈 카드 탭 axios 수정 및 유저 확인

--- frontend/dream/src/components/night/profile/NightProfileCardTab.tsx
+++ frontend/dream/src/components/night/profile/NightProfileCardTab.tsx
@@ -5,6 +5,7 @@
 // 리액트
 import React, { useEffect, useState } from "react";
 import tokenHttp from "api/tokenHttp";
+import { useSelector } from "react-redux";
 
 // 컴포넌트
 import InfiniteScroll from "components/common/InfiniteScroll";
@@ -14,6 +15,8 @@ import Button from "components/common/Button";
 import Image from "style/Image";
 import styled from "styled-components";
 import Text from "style/Text";
+import { useParams } from "react-router-dom";
+import { RootState } from "store";
 
 const ProfileCardButtonWrap = styled.div`
   margin: 1rem 0.5rem;
@@ -39,11 +42,19 @@ export interface ProfileDreamCardAxiosType {
 }
 
 const NightProfileCardTab = () => {
+  const params = useParams();
+  const userId = useSelector((state: RootState) => state.auth.userdata.userId); // 내 아이디
+  const [isMyProfile, setIsMyProfile] = useState<boolean>(false);
+
   // axios로 보내야 할 데이터
-  const [profileId, setProfileId] = useState<number>(1);
-  const [lastItemId, setLastItemId] = useState<number>(-1);
+  const profileId :number = Number(params.userId);
+  const [lastItemId, setLastItemId] = useState<number>(0);
   const [noCardMsg, setNoCardMsg] = useState<string>("");
+  const [hasNext, setHasNext] = useState<boolean>(true); 
   let size = 6;
+  
+  // Infinite scroll
+  const [arriveEnd, setArriveEnd] = useState<boolean>(true);
   
   // axios로 받아서 업데이트 할 데이터
   const [dreamCardList, setDreamCardList] = useState<ProfileDreamCardAxiosType[]>([]);
@@ -52,26 +63,30 @@ const NightProfileCardTab = () => {
     let apiAddress :string = "";
 
     // 처음 요청 받을 때 : lastItemId 없음
-    if (lastItemId === -1) {apiAddress = `/profile/night/card/${profileId}?size=${size}`}
+    if (lastItemId === 0) {apiAddress = `/profile/night/card/${profileId}?size=${size}`}
     // 두번째부터 요청 할 때
     else {apiAddress = `/profile/night/card/${profileId}?lastItemId=${lastItemId}&size=${size}`}
     
-    tokenHttp.get(apiAddress)
-    .then((res) => {
-      console.log("밤 꿈 카드 탭 : ", res.data)
-      
-      // 생성된 꿈카드가 있을 때
-      if (res.data.status === 200) {
-        // let newData = res.data.data.dreamCardList
-        setDreamCardList([...dreamCardList, ...res.data.data.dreamCardList]);
-        setLastItemId(res.data.data.dreamCardList[res.data.data.dreamCardList.length - 1].dreamCardId) 
-      } 
-      // 생성된 꿈카드가 없을 때
-      if (res.data.status === 400) {
-        setNoCardMsg(res.data.data);
-      }
-    })
-    .catch((err) => console.log(err))
+    if (arriveEnd && hasNext) {  // 끝에 도달하고 다음이 있을 때 다음 데이터 호출
+      tokenHttp.get(apiAddress)
+      .then((res) => {
+        console.log("밤 꿈 카드 탭 : ", res.data)
+        const response = res.data
+        const data = response.data
+
+        // 생성된 꿈카드가 있을 때
+        if (response.status === 200) {
+          const dreamCardList = data.dreamCardList
+          setDreamCardList([...dreamCardList, ...dreamCardList]);
+          setLastItemId(dreamCardList[dreamCardList.length - 1].dreamCardId) 
+        } 
+        // 생성된 꿈카드가 없을 때
+        if (response.status === 400) {
+          setNoCardMsg(data);
+        }
+      })
+      .catch((err) => console.log("밤 꿈 카드 탭 에러 : ", err))
+    }
   }
 
   useEffect(() => {
@@ -83,7 +98,7 @@ const NightProfileCardTab = () => {
 
   const handleShowCard = () => {
     setIsShowAllCard(!isShowAllCard)
-    console.log(isShowAllCard)
+    // console.log(isShowAllCard)
   }
 
   // 로딩 중
@@ -95,29 +110,36 @@ const NightProfileCardTab = () => {
   }, [dreamCardList]);
 
 
-  // Infinite scroll
-  const [arriveEnd, setArriveEnd] = useState<boolean>(false); // 바닥에 다다름을 알려주는 변수
-  
   // 바닥에 다다랐으면 axios 요청
   useEffect(() => {
     if (arriveEnd) {
-      // axios 요청
       getAxios();
       setArriveEnd(false);
     }
   }, [arriveEnd])
 
+  // 내 프로필인지 확인
+  useEffect(() => {
+    if (params && userId === Number(params.userId)) {
+      setIsMyProfile(true)
+      // console.log(isMyProfile, "=====")
+    }
+  }, [userId, params])
+
   return (
     <>
     {/* 프로필 유저 === 본인 일치 시 버튼 보이도록 */}
     {/* 공개, 구매 버튼 */}
-    <ProfileCardButtonWrap>
-      <Button 
-      $follow $nightPalePurple
-      onClick={handleShowCard}
-      >공개</Button>
-      <Button $follow $nightPalePurple>구매</Button>
-    </ProfileCardButtonWrap>
+    {
+      isMyProfile && 
+      <ProfileCardButtonWrap>
+        <Button 
+        $follow $nightPalePurple
+        onClick={handleShowCard}
+        >공개</Button>
+        <Button $follow $nightPalePurple>구매</Button>
+      </ProfileCardButtonWrap>
+    }
 
     {/* 로딩 중일 때 로딩 메시지 표시 */}
     {isLoading && <div>Loading...</div>}
@@ -134,9 +156,9 @@ const NightProfileCardTab = () => {
             dreamCardList
             .filter((card) => (isShowAllCard ? card.isShow === "T" : true))
             .map((card, i) => (
-                  <Image $profileCard $nightImageBorder key={i}>
-                    <img src={card.dreamCardImageUrl} alt="dreamCard"></img>
-                  </Image>
+              <Image $profileCard $nightImageBorder key={i}>
+                <img src={card.dreamCardImageUrl} alt="dreamCard"></img>
+              </Image>
             ))
             }
           > 
