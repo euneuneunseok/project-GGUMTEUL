5f3f9879d5bcf4d7cfb29760bb3eba5ad5b7eaf3
SquareOf_y <y6357@naver.com>
2023-10-02 20:54:34 +0900

    Fix: api/day 데이터 오류 처리

--- backend/dream/src/main/java/dream/challenge/service/ChallengeService.java
+++ backend/dream/src/main/java/dream/challenge/service/ChallengeService.java
@@ -53,6 +53,8 @@ public class ChallengeService {
 
         int count = 0;
         for (Challenge challenge : challenges) {
+            log.info("challengeId :{}", challenge.getChallengeId());`
+            log.info("challenge : {}", challenge.getKeywords().toString());
             responseChallengeList.add(ResponseChallenge.from(challenge));
 
             if (++count == size) break;
