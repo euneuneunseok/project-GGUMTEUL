3af5e6ecb9e0caef98decbc1fb1e4f0b151994fe
Min <dl_skflsalfm@naver.com>
2023-10-04 02:10:15 +0900

    [DE] 번역 및 완벽

--- dataend/main.py
+++ dataend/main.py
@@ -14,14 +14,16 @@ from pydantic import BaseModel
 # 내가 만든 함수
 from wordExtractService import getDreamKeywords
 from emotionAnalysisService import getEmotionScore
-from getKarlo import getKarloImgPath
+from getKarlo import getKarloImgPath, translateDreamKeywords
 
 from tempfile import NamedTemporaryFile
+
+from requests_toolbelt import MultipartEncoder
 
 # 꿈 데이터
 class DreamModel(BaseModel):
     dreamCardContent: str
-    dreamCardAuthor: float
+    dreamCardAuthor: int
     isShow: str
 
 app = FastAPI()
@@ -58,9 +60,46 @@ def root():
 
 # exit()
 
+# @app.post("/data/night/dream/create")
+# def dreamProcessing(data: DreamModel):
+#     # 받은 데이터 처리
+#     print(data, "널 좀 알고 싶다...!")
+#     dreamCardContent = data.dreamCardContent
+#     dreamCardAuthor = data.dreamCardAuthor
+#     isShow = data.isShow
+#     wordKeywords = getDreamKeywords(dreamCardContent)
+#     positivePoint, negativePoint = getEmotionScore(dreamCardContent)
+    
+#     # 임시로 넣음. 원래는 번역한 텍스트를 넣어야 해.
+#     prompt = "A cat with white fur, floating balloon, by Renoir"
 
+#     img_path = getKarloImgPath(prompt)
+#     print("files 앞", img_path)
+#     # files = {'file': ("karloImage.png", open(img_path, 'rb'), "image/png")} #
 
+#     # 여긴 내 가정. (파일 전용 경로 필요)
+#     # 내거 마지막
 
+#     # 이걸 대신하기.
+#     # print("files 뒤", files)
+
+#     toJavaData = {
+#         "dreamCardDetail": {        
+#             "dreamCardContent": dreamCardContent,
+#             "dreamCardAuthor": dreamCardAuthor,
+#             "isShow": isShow,
+#             "positivePoint": positivePoint,
+#             "negativePoint": negativePoint,
+#             "keywords": ["학업", "재물"],
+#             "wordKeywords": wordKeywords
+#         }
+#     }
+#     print(toJavaData, "자바로 갈 데이터")
+#     response = requests.post('https://j9b301.p.ssafy.io/api/s3/dream/new', json=toJavaData, headers=headers)
+#     data = response.json()
+#     print(data, "데이터")
+#     dreamCardId = data["data"]    
+#     return response
 @app.post("/data/night/dream/create")
 def dreamProcessing(data: DreamModel):
     # 받은 데이터 처리
@@ -70,41 +109,30 @@ def dreamProcessing(data: DreamModel):
     isShow = data.isShow
     wordKeywords = getDreamKeywords(dreamCardContent)
     positivePoint, negativePoint = getEmotionScore(dreamCardContent)
-    
+
     # 임시로 넣음. 원래는 번역한 텍스트를 넣어야 해.
-    prompt = "A cat with white fur, floating balloon, by Renoir"
+    prompt = translateDreamKeywords(wordKeywords) + ", by Renoir"
+    # prompt = "A cat with white fur, floating balloon, by Renoir"
 
     img_path = getKarloImgPath(prompt)
-    print("files 앞", img_path)
-    # files = {'file': ("karloImage.png", open(img_path, 'rb'), "image/png")} #
-
-    # 여긴 내 가정. (파일 전용 경로 필요)
-    # 내거 마지막
-
-    # 이걸 대신하기.
-    # print("files 뒤", files)
 
     toJavaData = {
-        "dreamCardDetail": {        
-            "dreamCardContent": dreamCardContent,
-            "dreamCardAuthor": dreamCardAuthor,
-            "isShow": isShow,
-            "positivePoint": positivePoint,
-            "negativePoint": negativePoint,
-            "keywords": ["학업", "재물"],
-            "wordKeywords": wordKeywords
-        }
+        "dreamCardContent": dreamCardContent,
+        "dreamCardAuthor": dreamCardAuthor,
+        "isShow": isShow,
+        "positivePoint": positivePoint,
+        "negativePoint": negativePoint,
+        "keywords": ["학업", "재물"],
+        "wordKeywords": wordKeywords
     }
     print(toJavaData, "자바로 갈 데이터")
-    response = requests.post('https://j9b301.p.ssafy.io/api/s3/dream/new', data=toJavaData, headers=headers)
-    print(response["data"], "응답!")
-    print(response["data"]["dreamCardId"], "번호..?!")
-    dreamCardId = response["data"]["dreamCardId"]
+    response = requests.post('https://j9b301.p.ssafy.io/api/s3/dream/new', json=toJavaData, headers=headers)
+    data = response.json()
+    print(data, "data~")
+    dreamCardId = data["data"]["dreamCardId"]
 
     with open(img_path, "rb") as file:
-        files = {"file": (img_path, file)}
-    requests.post(f"https://j9b301.p.ssafy.io/api/s3/dream/image/{dreamCardId}", files=files, headers=file_headers)
-    print(files, "files#########")
-    
-    return response
+        files = {"file": ("karloImage.png", file.read())}
+        requests.post(f"https://j9b301.p.ssafy.io/api/s3/dream/image/{dreamCardId}", files=files)
+        return f"{dreamCardId}"
 
